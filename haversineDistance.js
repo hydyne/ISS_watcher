6860d344a212a841f5439c602dfddcf54475ab01
Mateusz <mateuszkarpeta@gmail.com>
2024-09-02 10:51:28 +0200

    add distance ISS location to selected city

---
+++ haversineDistance.js
@@ -0,0 +1,16 @@
+function haversineDistance(lat1, lon1, lat2, lon2) {
+    const toRadians = (degree) => degree * (Math.PI / 180);
+
+    const R = 6371; // Radius of the Earth in kilometers
+    const dLat = toRadians(lat2 - lat1);
+    const dLon = toRadians(lon2 - lon1);
+
+    const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+              Math.cos(toRadians(lat1)) * Math.cos(toRadians(lat2)) *
+              Math.sin(dLon / 2) * Math.sin(dLon / 2);
+
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    const distance = R * c; // Distance in kilometers
+    return distance;
+}
